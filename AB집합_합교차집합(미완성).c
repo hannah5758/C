11187288dc38ac539a78a5f296825d14dbc9efc8
Hannah <115331132+hannah5758@users.noreply.github.com>
2022-10-26 18:52:33 +0900

    Update AB집합_합교차집합(미완성).c

--- AB집합_합교차집합(미완성).c
+++ AB집합_합교차집합(미완성).c
@@ -3,6 +3,7 @@
 int main(void) {
     int a[5], b[5];
     int i;
+    int sum_arr[10];
     
     printf("A 집합의 원소 5개 입력 : \n");
     for (i=0;i<5;i++){
@@ -15,6 +16,28 @@ int main(void) {
     }
     
     //합집합
+    //합집합 a b 중복되는건 하나만
+    for (int i=0; i < 5; i++){
+        sum_arr[i] = a[i];
+        sum_arr[i+5] = b[i];
+    }
+    
+    for (int i=0; i < 10; i++){
+        printf("%d", sum_arr[i]);
+    }
+    
+    for (int i = 0; i < 5; i++){
+        int same_count = 0;
+        for (int j = 0; j < 5 ; j++){
+            if (a[i] == b[j]){
+                printf("%d", a[i]);
+                same_count = same_count + 1;
+            }
+        }
+        if (same_count == 0){
+            printf("%d", a[i]);
+        }
+    }
     printf("A와 B의 합집합 : \n");
 
     return 0;
